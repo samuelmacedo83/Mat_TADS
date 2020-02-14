f6e5b798f5d359632de7c97628f867f7eeccc402
renatovanderlei <nato_vanderlei@hotmail.com>
2020-02-14 12:04:41 -0300

    Script aula Mat Aplicada TADS dply

---
+++ Script TADS dplyr.R
@@ -0,0 +1,12 @@
+install.packages("dplyr")
+library(dplyr)
+
+iris
+glimpse(iris)
+
+iris %>%  #ctr+shift+m= novo pipe
+  filter(Petal.Length >2) %>% #filtra pétalas acima de 2
+  select(Species) #seleciona apenas espécies com pétalas acima de 2
+
+mutate(iris, Petal.Length2=Petal.Length+2) #cria nova coluna depois da última com nome dado + informação inserida.
+
